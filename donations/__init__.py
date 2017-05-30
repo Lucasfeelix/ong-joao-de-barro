bc8a7e9b500a923812867946c0cdcf75f717fe2d
lucas <lucas.felix@gastecnologia.com>
2017-05-30 14:18:10 -0300

    Alteração de nomes em __init__.py

--- donations/__init__.py
+++ donations/__init__.py
@@ -1,4 +1,4 @@
 # coding=utf-8
 
 default_app_config = 'donations.apps.DonationsConfig'
-# altera o nome padrão da app, passando de 'catalog' para 'Catálogo'
+# altera o nome padrão da app, passando de 'Donations' para 'Doações'
