6b732b4c9b8f096c27822d1bd92c8e4a73b48210
Lucas Felix <lucasfelix225@hotmail.com>
2017-06-13 22:43:07 -0300

    Adição app expenses

---
+++ core/helpers/multiple_choices.py
@@ -0,0 +1,9 @@
+
+EXPENSES_TYPE = ((u'Aluguel', u'Aluguel'),
+                 (u'Compra', u'Compra'),
+                 (u'Devolução', u'Devolução'),
+                 (u'Extorno', u'Extorno'),
+                 (u'Pagamento', u'Pagamento'))
+
+DONATIONS_TYPE = ((u'Doação', u'Doação'),
+                  (u'Recebimento', u'Recebimento'))
---
+++ expenses/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from core.models import TimeModel
+from core.helpers.multiple_choices import EXPENSES_TYPE
+
+
+class Expenses(TimeModel):
+    service_type = models.CharField('Tipo de serviço', max_length=12,
+                                    choices=EXPENSES_TYPE, default='Compra')
+    item = models.CharField('Item', max_length=100, default='')
+    description = models.TextField('Descrição', blank=True)
+    quantity = models.IntegerField('Quantidade', default=0)
+    price = models.DecimalField('Preço', max_digits=6, decimal_places=2,
+                                default=0)
+    date = models.DateTimeField('Data', auto_now_add=True)
+
+    @property
+    def total(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return str(self.service_type)
+
+    class Meta:
+        ordering = ['service_type']
+        verbose_name = 'Despesa'
+        verbose_name_plural = 'Despesas'
+
+    def get_absolute_url(self):
+        return reverse('donations:donation_detail', kwargs={'pk': self.pk})
